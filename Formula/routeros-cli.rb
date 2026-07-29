aa496c89e3545a27d366768dafcd62c55e20152c
Ignacio Medina <ignacionmedina1@gmail.com>
2026-07-29 18:26:51 -0300

    chore: bump AUR, Scoop, and Formula packaging to v0.5.0

--- Formula/routeros-cli.rb
+++ Formula/routeros-cli.rb
@@ -2,27 +2,27 @@ class Ros < Formula
   desc "CLI tool for managing MikroTik RouterOS routers with structured JSON output"
   homepage "https://github.com/nic0der-im/routeros-cli"
   license "MIT"
-  version "0.4.0"
+  version "0.5.0"
 
   on_macos do
     on_arm do
       url "https://github.com/nic0der-im/routeros-cli/releases/download/v#{version}/ros_#{version}_darwin_arm64.tar.gz"
-      sha256 "53a1ec04b461a430b19e657b2ae0e8cf784a857ecdb9ea1f46634f9753b76272"
+      sha256 "46c524a8973b9da7d2a57c62b5a761698824cdc628f0ed05a1d31d2ec870f1b8"
     end
     on_intel do
       url "https://github.com/nic0der-im/routeros-cli/releases/download/v#{version}/ros_#{version}_darwin_amd64.tar.gz"
-      sha256 "3b87961eb3d0d53059b5c23d727f7c7d7b150065833e4dcb2bc337fe72d3b05f"
+      sha256 "53b9a953a7c575492a3ee8fd59cc78893241ede9dcf8e28c4c6bdfea33fce74f"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/nic0der-im/routeros-cli/releases/download/v#{version}/ros_#{version}_linux_arm64.tar.gz"
-      sha256 "7cf09ed8f491bb67101e686d3b884c0a73ab67781ebbdd0dff0d3c358e203b83"
+      sha256 "903fedd76555ff8d51e2711677e72ff33dcf2eb70fc655d9a96486e7057a1f8a"
     end
     on_intel do
       url "https://github.com/nic0der-im/routeros-cli/releases/download/v#{version}/ros_#{version}_linux_amd64.tar.gz"
-      sha256 "ee6325e1012e4da76d182f6241b3b5a9985e394d17b2d005683bc961f3631e25"
+      sha256 "54e6a8e741d28eb8f57f443c7826a111be064e37ab97000ff3d2a49f7432e67f"
     end
   end
 
